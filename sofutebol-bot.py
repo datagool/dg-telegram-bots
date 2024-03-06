4c4eb155cf98ddccf399df2b5920500cba21085a
Everton Tenorio <114461163+everton-tenorio@users.noreply.github.com>
2024-03-06 04:48:50 -0300

    Update sofutebol-bot.py

--- sofutebol-bot.py
+++ sofutebol-bot.py
@@ -74,10 +74,9 @@ def jogos_por_dia_callback(update: Update, context: CallbackContext) -> None:
 
     if 0 <= dia_escolhido_index < len(jogos):
         jogos_para_dia_escolhido = jogos[dia_escolhido_index]
-        mensagem_formatada = formatar_jogos_para_dia(jogos_para_dia_escolhido)
+        msg_format = formatar_jogos_para_dia(jogos_para_dia_escolhido)
         context.bot.send_message(
-            chat_id=query.message.chat_id, text=mensagem_formatada,
-            parse_mode="HTML"
+            chat_id=query.message.chat_id, text=msg_format, parse_mode="HTML"
         )
     else:
         context.bot.send_message(
@@ -87,12 +86,12 @@ def jogos_por_dia_callback(update: Update, context: CallbackContext) -> None:
 
 def help_command(update, context):
     available_commands = [
-        "/jogos - Mostra os jogos de hoje\n",
+        "/jogos - Mostra os jogos de hoje\n"
         "/help - Lista de comandos disponíveis",
     ]
     help_message = "Comandos disponíveis:\n" + "\n".join(available_commands)
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=help_message)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text=help_message)
 
 
 # Adicione o manipulador de comandos para o comando /jogos
@@ -102,8 +101,8 @@ dispatcher.add_handler(CommandHandler("jogos", jogos_hoje))
 dispatcher.add_handler(CallbackQueryHandler(jogos_por_dia_callback))
 
 dispatcher.add_handler(CommandHandler("help", help_command))
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command,
-                                      help_command))
+dispatcher.add_handler(MessageHandler(
+    Filters.text & ~Filters.command, help_command))
 
 # Inicie o bot
 updater.start_polling()
